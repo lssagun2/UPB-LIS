873a1f2fcd43c2bdae599499edcd2ff857c5d45f
chrlscrdl <71239226+chrlscrdl@users.noreply.github.com>
2021-05-14 10:15:33 -0500

    Formhandler (with form validation)
    1. Reset form after submitting inputs.
    2. Adding class "error" and "submit" after submitting inputs.

--- staff/js/formhandler.js
+++ staff/js/formhandler.js
@@ -1,5 +1,10 @@
+function update(){
+	$("table#inventory").load(encodeURI("functions/update.php"));
+}
 $(document).ready(function(){
 	$("form#staff").submit(function(event){
+		$('div.form-control.error').removeClass('error');
+		$('div.form-control.success').removeClass('success');
 		event.preventDefault();
 		$(".input-invalid").hide();
 		var function_name = $("#staff_function").val();
@@ -10,8 +15,56 @@ $(document).ready(function(){
 			dataType 	: 'json'
 		})
 		.done(function(data){
-			console.log(data);
-			$('div.modal').hide();
+			if(data.success){
+				console.log(data);
+				console.log($('form#staff').serialize());
+				$('div.form-control.error').removeClass('error');
+				$('div.form-control.success').removeClass('success');
+				$('div.modal').hide();
+				update();
+			}
+			else{
+				console.log(data);
+				console.log($('form#staff').serialize());
+				if(data.errors.staff_username){
+					$('div#usernameform').addClass('error');
+					$('div#usernameform small').html(data.errors.staff_username);
+				}
+				else{
+					$('div#usernameform').addClass('success');
+				}
+				if(data.errors.staff_firstname){
+					$('div#firstnameform').addClass('error');
+					$('div#firstnameform small').html(data.errors.staff_firstname);
+				}
+				else{
+					$('div#firstnameform').addClass('success');
+				}
+				if(data.errors.staff_lastname){
+					$('div#lastnameform').addClass('error');
+					$('div#lastnameform small').html(data.errors.staff_lastname);
+				}
+				else{
+					$('div#lastnameform').addClass('success');
+				}
+				if(data.errors.staff_password){
+					$('div#passwordform').addClass('error');
+					$('div#passwordform small').html(data.errors.staff_password);
+				}
+				else{
+					$('div#passwordform').addClass('success');
+				}
+				if(data.errors.confirm_password){
+					$('div#confirmpasswordform').addClass('error');
+					$('div#confirmpasswordform small').html(data.errors.confirm_password);
+				}
+				else{
+					$('div#confirmpasswordform').addClass('success');
+				}
+
+			}
+			
+
 		})
 		.fail(function(data) {
 	      //Server failed to respond - Show an error message
