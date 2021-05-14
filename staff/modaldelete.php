b05639e49990ae5bdd6fe5e4db6181d6cedfb109
chrlscrdl <71239226+chrlscrdl@users.noreply.github.com>
2021-05-14 10:26:03 -0500

    Modal for delete
    1. Di pa tapos

---
+++ staff/modaldelete.php
@@ -0,0 +1,23 @@
+<div class = "modal" id = "deleteStaff">
+  <?php 
+    // $sql = "SELECT * FROM STAFF WHERE staff_id = '$primaryKey'";
+    // $result = $conn->query($sql);
+    // $row = $result->fetch_assoc();
+  ?>
+  <span class = "close" title = "Close Modal"><i class="fas fa-times"></i></span>
+  <form class = "modal-content" id = "staff" method = "POST">
+    <div class = "containerDelete" style = "overflow-y: auto; height: 480px;">
+      <h1 class = "modal-title">Update My Information</h1>
+      <input type="hidden" name="function" id = "staff_function" value = "">
+
+      <input type = "hidden" id = "staff_id" name = "staff_id" value = "">
+
+      
+
+      
+
+      <button type = "button" onclick = "$('div.modal').hide()" class = "modalbtn" id = "cancelbtn">Cancel</button>
+      <button type = "button" class = "modalbtn" id = "edit-staff">Save changes</button>
+    </div>
+  </form>
+</div>
