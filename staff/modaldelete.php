bab1a8aa0faabcc0f1efbce0347531ceab10628c
tpalmonia <71270543+tpalmonia@users.noreply.github.com>
2021-05-19 02:34:05 +0800

    Changes in Delete modal

--- staff/modaldelete.php
+++ staff/modaldelete.php
@@ -6,25 +6,22 @@
   ?>
   <span class = "close" title = "Close Modal"><i class="fas fa-times"></i></span>
   <form class = "modal-content" id = "deleteStaffForm" method = "POST">
-    <div class = "containerDelete" style = "overflow-y: auto; height: 480px;">
+    <div class = "containerDelete" style = "overflow-y: auto; height: 404px;">
       <h1 class = "modal-title"></h1>
       <div>
-      <strong>Are you sure you want to delete this data?</strong>
+        <strong>Are you sure you want to delete this data?</strong> <br><br>
       </div>
 
       <input type="hidden" readonly="readonly" name="primaryKey" id = "primaryKey" val = "">
-      
-      <div class="form-control"> 
+    
       <label for="firstnamedel">Username</label>
       <input type = "text" readonly="readonly" name="firstnamedel" id = "firstnamedel" val = "">
-      </div>
       <label for="lastnamedel">Last Name</label>
       <input type = "text" readonly="readonly" name="lastnamedel" id = "lastnamedel" val = "">
       <label for="passworddel">Password</label>
       <input type = "text" readonly="readonly" name="passworddel" id = "passworddel" val = "">
       <button type = "button" onclick = "$('div.modal').hide()" class = "modalbtn" id = "cancelbtn">Cancel</button>
       <button type = "button" class = "modalbtn" id = "delete-staff">Delete Data</button>
-
     </div>
   </form>
 </div>
