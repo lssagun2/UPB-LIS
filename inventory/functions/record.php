c6d688b39e6c905fdd55908bf3cd5415dae56cbf
Adrianne Aniban <65640407+aianiban@users.noreply.github.com>
2021-05-20 12:53:56 +0800

    record.php with edit function via save changes
    1. Clicking save changes now edits the material

--- inventory/functions/record.php
+++ inventory/functions/record.php
@@ -2,13 +2,17 @@
 	session_start();
 	require $_SERVER['DOCUMENT_ROOT']."/upb-lis/config.php";
 	require $_SERVER['DOCUMENT_ROOT']."/upb-lis/functions/edit.php";
+	require $_SERVER['DOCUMENT_ROOT']."/upb-lis/functions/add.php";
 	date_default_timezone_set('Asia/Manila');
 
 	if (isset($_POST["acc_num"])){
 	$year = date("Y");
 	$mat_acc_num = trim($_POST["acc_num"]);
-	$sql = "SELECT mat_id FROM MATERIAL WHERE mat_acc_num = '$mat_acc_num'";
+	$sql = "SELECT * FROM MATERIAL WHERE mat_acc_num = '$mat_acc_num'";
 	$result = $conn->query($sql);
+	$data = []; //changes
+
+
 	if($result->num_rows != 0){
 		$row = $result->fetch_assoc();
 		$mat_id = $row['mat_id'];
@@ -18,15 +22,35 @@
 		//error handling
 	}
 	$inv_info = [
-		"inv_$year" => 1,
-		"date_$year" => date("Y-m-d H:i:s"),
-		"staff_id_$year" => $_SESSION["staff_id"]
+				'mat_acc_id' => $row['mat_id'],
+				"inv_$year" => 1,
+				"date_$year" => date("Y-m-d H:i:s"),
+				"staff_id_$year" => $_SESSION["staff_id"]
 	];
+	$info=[
+				"mat_acc_num" => trim($_POST['acc_num']),
+			 	"mat_barcode" => trim($_POST['barcode']),
+			 	"mat_call_num" => trim($_POST['call_number']),
+			 	"mat_title" => trim($_POST['title']),
+			 	"mat_author" => trim($_POST['author']),
+			 	"mat_volume" => trim($_POST['volume']),  
+			 	"mat_year" => trim($_POST['year']), 
+			 	"mat_edition" => trim($_POST['edition']), 
+			 	"mat_publisher" => trim($_POST['publisher']),
+			 	"mat_pub_year" => trim($_POST['pub_year']),
+			 	"mat_circ_type" => trim($_POST['circ_type']),
+			 	"mat_type" => trim($_POST['type']),
+			 	"mat_status" => trim($_POST['status']),
+			 	"mat_source" => trim($_POST['source']), 
+			 	"mat_location" => trim($_POST['location']),
+			 	"mat_lastinv_year" => $year
+			 ];
+	add($conn, "INVENTORY", $inv_info);
 	edit($conn, "INVENTORY", $mat_id, $inv_info);
-	$update_info = [
-		"mat_lastinv_year" => $year
-	];
-	edit($conn, "MATERIAL", $mat_id, $update_info);
+	edit($conn, "MATERIAL", $mat_id, $info);
+
+
+
 	$data["success"] = true;
 	echo json_encode($data);
 	}
