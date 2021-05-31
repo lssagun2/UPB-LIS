7ccb756a408cb52ed4172e55109128aaf86f37a4
gdderije <71222985+gdderije@users.noreply.github.com>
2021-05-31 22:54:02 +0800

    Filter form padding and select width

--- report/modal.php
+++ report/modal.php
@@ -1,10 +1,10 @@
 <div class = "modal" id = "filter">
   <span class = "close" title = "Close Modal"><i class="fas fa-times"></i></span>
-  <form class = "modal-content" id = "filter-form" method = "POST">
+  <form class = "modal-content" id = "filter-form" method = "POST" style = "padding: 20px">
     <div class = "container" style = "overflow-y: auto;">
     <h1 class = "modal-title"></h1>
       <label for="circtype-filter">Circulation Type:</label>
-      <select class = "filter-column" id="circtype-filter" name="circtype-filter">
+      <select class = "filter-column" id="circtype-filter" name="circtype-filter" style = "width: 98%;">
         <option value = "" selected = "selected">---none---</option>
         <?php
           $sql = "SELECT DISTINCT mat_circ_type FROM MATERIAL";
@@ -15,7 +15,7 @@
         <?php } ?>
       </select>
       <label for="type-filter">Material Type:</label>
-      <select class = "filter-column" id="type-filter" name="type-filter">
+      <select class = "filter-column" id="type-filter" name="type-filter" style = "width: 98%;">
         <option value = "" selected = "selected">---none---</option>
         <?php
           $sql = "SELECT DISTINCT mat_type FROM MATERIAL";
@@ -26,7 +26,7 @@
         <?php } ?>
       </select>
       <label for="status-filter">Status:</label>
-      <select class = "filter-column" id="status-filter" name="status-filter">
+      <select class = "filter-column" id="status-filter" name="status-filter" style = "width: 98%;">
         <option value = "" selected = "selected">---none---</option>
         <?php
           $sql = "SELECT DISTINCT mat_status FROM MATERIAL";
@@ -37,7 +37,7 @@
         <?php } ?>
       </select>
       <label for="location-filter">Location:</label>
-      <select class = "filter-column" id="location-filter" name="location-filter">
+      <select class = "filter-column" id="location-filter" name="location-filter" style = "width: 98%;">
         <option value = "" selected = "selected">---none---</option>
        <?php
           $sql = "SELECT DISTINCT mat_location FROM MATERIAL";
