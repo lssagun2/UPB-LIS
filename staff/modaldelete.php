601f4073dbb595d0e005f0e278e2c36f898b06b0
gdderije <71222985+gdderije@users.noreply.github.com>
2021-06-04 12:41:29 +0800

    Rearrange buttons

--- staff/modaldelete.php
+++ staff/modaldelete.php
@@ -1,5 +1,5 @@
 <div class = "modal" id = "deleteStaff">
-  <?php 
+  <?php
     // $sql = "SELECT * FROM STAFF WHERE staff_id = '$primaryKey'";
     // $result = $conn->query($sql);
     // $row = $result->fetch_assoc();
@@ -13,15 +13,15 @@
       </div>
 
       <input type="hidden" readonly="readonly" name="primaryKey" id = "primaryKey" val = "">
-    
+
       <label for="firstnamedel">Username</label>
       <input type = "text" readonly="readonly" name="firstnamedel" id = "firstnamedel" val = "">
       <label for="lastnamedel">Last Name</label>
       <input type = "text" readonly="readonly" name="lastnamedel" id = "lastnamedel" val = "">
       <label for="passworddel">Password</label>
       <input type = "text" readonly="readonly" name="passworddel" id = "passworddel" val = "">
-      <button type = "button" onclick = "$('div.modal').hide()" class = "modalbtn" id = "cancelbtn">Cancel</button>
       <button type = "button" class = "modalbtn" id = "delete-staff">Delete Data</button>
+      <button type = "button" onclick = "$('div.modal').hide()" class = "modalbtn" id = "cancelbtn">Cancel</button>
     </div>
   </form>
 </div>
