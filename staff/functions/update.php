d816a217aa671b22512d93492744e873820773ad
gdderije <71222985+gdderije@users.noreply.github.com>
2021-06-06 16:34:02 +0800

    Table update

--- staff/functions/update.php
+++ staff/functions/update.php
@@ -3,12 +3,12 @@
 ?>
   <thead>
     <tr>
-      <th width="15%" style = "border-radius: 1em 0 0 0;">Username</th>
-      <th width="20%">First Name</th>
-      <th width="25%">Last Name</th>
-      <th width="20%">Password</th>
-      <th width="20%">Type</th>
-      <th width="10%" style = "border-radius: 0 1em 0 0;">Action</th>
+			<th width="15%" style = "border-radius: 0.9em 0 0 0;">Username</th>
+			<th width="20%">First Name</th>
+			<th width="20%">Last Name</th>
+			<th width="20%">Password</th>
+			<th width="15%">Type</th>
+			<th width="10%" style = "border-radius: 0 0.9em 0 0;">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -18,17 +18,17 @@
       while($row = $result->fetch_assoc()){
     ?>
   <tr align="center">
-    <td style='display:none;'><?php echo $row['staff_id'] ?></td> 
+    <td style='display:none;'><?php echo $row['staff_id'] ?></td>
     <td><?php echo $row['staff_username'] ?></td>
     <td><?php echo $row['staff_firstname'] ?></td>
     <td><?php echo $row['staff_lastname'] ?></td>
     <td><?php echo $row['staff_password'] ?></td>
     <td><?php echo $row['staff_type'] ?></td>
-                    
-    <td>
-      <button id = 'deleteStaffTable' class = 'edit' style = 'width: 50px; height: 50px; color: #000;'><i class="fas fa-trash-alt"></i></button>
-      <button id = 'editStaffTable' class = 'edit' style = 'width: 50px; height: 50px; color: #000;'><i class = 'fas fa-edit'></i></button>
-    </td>
+
+		<td style = "margin-right: 5px;" align = "center">
+			<button id = 'editStaffTable' class = 'edit'><i class = 'fas fa-edit'></i></button>
+			<button id = 'deleteStaffTable' class = 'edit'><i class="fas fa-trash-alt"></i></button>
+		</td>
   </tr>
     <?php } ?>
   </tbody>
