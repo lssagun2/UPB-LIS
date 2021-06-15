54ef4538efbb6ea13af4a9c3125053f1e4ba89bc
Adrianne Aniban <65640407+aianiban@users.noreply.github.com>
2021-06-15 12:40:51 +0800

    Update restore function
    1. Drop tables before restore call so that edits are reflected.

--- backup and restore/modal.php
+++ backup and restore/modal.php
@@ -68,6 +68,17 @@ if (! empty($_FILES)) {
 
 function restore($filePath, $conn)
 {
+$mysqli = new mysqli("localhost", "root", "", "UPB_LIS");
+$mysqli->query('SET foreign_key_checks = 0');
+if ($result = $mysqli->query("SHOW TABLES"))
+{
+    while($row = $result->fetch_array(MYSQLI_NUM))
+    {
+        $mysqli->query('DROP TABLE IF EXISTS '.$row[0]);
+    }
+}
+
+$mysqli->query('SET foreign_key_checks = 1');
     $sql = '';
     $error = '';
 
