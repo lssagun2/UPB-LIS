a78249421dd04c5aaec292fdf9dfe371e3d4732a
Leandrei Sagun <lssagun2@up.edu.ph>
2021-06-27 22:41:49 +0800

    Create new columns at config

--- config.php
+++ config.php
@@ -7,4 +7,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$year = date("Y");
+$sql = "SHOW COLUMNS FROM INVENTORY LIKE '%_year'";
+$result = $conn->query($sql);
+if($result->num_rows == 0){
+  $sql = "ALTER TABLE INVENTORY ADD staff_id_$year INT AFTER mat_id";
+  $conn->query($sql);
+  $sql = "ALTER TABLE INVENTORY ADD date_$year DATETIME AFTER mat_id";
+  $conn->query($sql);
+  $sql = "ALTER TABLE INVENTORY ADD inv_$year INT AFTER mat_id";
+  $conn->query($sql);
+}
 ?>
